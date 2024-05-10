d1b5b4bc49524483bac6afc093c068e1cabab407
= <caseyjane2013@gmail.com>
2024-05-10 10:44:40 +0100

    updated master code to send image size,width and height before sending image bytes

--- I2C_test/I2C_master.py
+++ I2C_test/I2C_master.py
@@ -2,8 +2,6 @@ import smbus2
 import array
 from PIL import Image
 
-# SMBUS1 WORKED FINE FOR THE BELOW
-
 
 bus = smbus2.SMBus(1)
 
@@ -15,33 +13,31 @@ def sendString(slaveAddress, data):
 # sendString(0x03, "hello")
 
 
-# IGNORE THE BELOW, DOESNT WOKR YET
-
-# smbus 2 allows for 512 max bytes per msg, smbus1 allows for 32 only
+# smbus 2.i2c.rdwr allows for 512 max bytes per msg, smbus1 allows for 32 only
 
 image = Image.open('4kb_image.png')
 image_bytes = image.tobytes()
-
 image_size = len(image_bytes)
-print(image_size)
-# print(f'\N\N IMAGE SIZE {image_size}')
+image_width, image_height = image.size
+print("Image Byte Size: " + str(image_size))
+print("Image Width: " + str(image_width))
+print("Image height: " + str(image_height))
 
-image_size_array = array.array('i', [image_size])
-image_size_array_bytes = image_size_array.tobytes()
-#print(image_size_array_bytes)
 
-#print(image_bytes)
+image_size_bytes = image_size.to_bytes(4, byteorder='big')
+image_width_bytes = image_width.to_bytes(2, byteorder='big')
+image_height_bytes = image_height.to_bytes(2, byteorder='big')
+
+# send image size, width and height first
+bus.write_i2c_block_data(0x03, image_size_bytes[0], image_size_bytes[1:])
+bus.write_i2c_block_data(0x03, image_width_bytes[0], image_width_bytes[1:])
+bus.write_i2c_block_data(0x03, image_height_bytes[0], image_height_bytes[1:])
+
 
 def send_bytes(image_bytes):
-	
-    # for i in range(0, len(image_bytes), 32):
-    # bus.write_i2c_block_data(0x03,image_bytes[0], image_bytes[1:2])
-    # bus.write_i2c_block_data(0x03, image_bytes[0], list(image_bytes)[:64])
-    
     for i in range(0, len(image_bytes), 512):  
         bus.i2c_rdwr(smbus2.i2c_msg.write(0x03, list(image_bytes)[i: i + 512]))
 
 
-#send_bytes(image_size_array_bytes)
 send_bytes(image_bytes)
 
--- I2C_test/I2C_master_basic.py
+++
@@ -1,9 +0,0 @@
-import smbus
-
-bus = smbus.SMBus(1)
-
-def sendData(slaveAddress, data):
-    intsOfData = list(map(ord, data))
-    bus.write_i2c_block_data(slaveAddress, intsOfData[0], intsOfData[1:])
-
-# sendData(0x03, 'Hello World of I2C!')
